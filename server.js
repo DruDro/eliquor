4e904ea196436cd357b7501052cc85ac4fc52125
drudro <drudro@yandex.ru>
2018-03-25 04:31:45 +0300

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname,'src/db.json'));
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 server.use(middlewares);
 server.use(router);
