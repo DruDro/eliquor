2b18d9f2052c3a1acf1e50af2d98d05d18ed1668
drudro <drudro@yandex.ru>
2018-03-25 14:27:18 +0300

    angular - express router

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'dist')));
 app.use('/api', jsonServer.router('db.json'));
+app.get('*', (req,res) => res.sendFile(path.join(__dirname, 'dist/index.html')));
 const port = process.env.PORT || 4444;
 app.set('port', port);
 
